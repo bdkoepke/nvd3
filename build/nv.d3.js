37a1b6158ad528c09480407fffcf8c766d7cdb18
Jin Won Choi <jin.choi@moneygeek.ca>
2017-12-12 19:49:47 -0500

    added groupSpacing to discreteBar

--- build/nv.d3.js
+++ build/nv.d3.js
@@ -1,4 +1,4 @@
-/* nvd3 version 1.8.6-dev (https://github.com/novus/nvd3) 2017-12-08 */
+/* nvd3 version 1.8.6-dev (https://github.com/novus/nvd3) 2017-12-12 */
 (function(){
 
 // set up main nv object
@@ -4007,6 +4007,7 @@ nv.models.discreteBar = function() {
         , dispatch = d3.dispatch('chartClick', 'elementClick', 'elementDblClick', 'elementMouseover', 'elementMouseout', 'elementMousemove', 'renderEnd')
         , rectClass = 'discreteBar'
         , duration = 250
+        , groupSpacing = 0.1
         ;
 
     //============================================================
@@ -4042,7 +4043,7 @@ nv.models.discreteBar = function() {
                 });
 
             x   .domain(xDomain || d3.merge(seriesData).map(function(d) { return d.x }))
-                .rangeBands(xRange || [0, availableWidth], .1);
+                .rangeBands(xRange || [0, availableWidth], groupSpacing);
             y   .domain(yDomain || d3.extent(d3.merge(seriesData).map(function(d) { return d.y }).concat(forceY)));
 
             // If showValues, pad the Y axis range to account for label height
--- src/models/discreteBar.js
+++ src/models/discreteBar.js
@@ -26,6 +26,7 @@ nv.models.discreteBar = function() {
         , dispatch = d3.dispatch('chartClick', 'elementClick', 'elementDblClick', 'elementMouseover', 'elementMouseout', 'elementMousemove', 'renderEnd')
         , rectClass = 'discreteBar'
         , duration = 250
+        , groupSpacing = 0.1
         ;
 
     //============================================================
@@ -61,7 +62,7 @@ nv.models.discreteBar = function() {
                 });
 
             x   .domain(xDomain || d3.merge(seriesData).map(function(d) { return d.x }))
-                .rangeBands(xRange || [0, availableWidth], .1);
+                .rangeBands(xRange || [0, availableWidth], groupSpacing);
             y   .domain(yDomain || d3.extent(d3.merge(seriesData).map(function(d) { return d.y }).concat(forceY)));
 
             // If showValues, pad the Y axis range to account for label height
